bc0bbe9a85df481913c2c8a3e5a17aa397be6011
ygo74 <yannickgobert@yahoo.fr>
2023-06-22 00:50:09 +0200

    Try add new task

--- src/CleanSemanticVersionForDockerTagV1/index.ts
+++ src/CleanSemanticVersionForDockerTagV1/index.ts
@@ -1,9 +1,13 @@
-import { TaskResult, getInput, setOutput } from 'azure-pipelines-task-lib/task';
+import path = require('path');
+import tl = require('azure-pipelines-task-lib/task');
 
 async function run(): Promise<void> {
   try {
-    // Récupérer la version actuelle
-    const currentVersion: string = process.env.BUILD_BUILDNUMBER || '';
+    tl.debug('Start Task Run' );
+    tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+    // Read Mandatory inputs
+    const currentVersion: string = tl.getInput('semanticVersioning', true) || '';
 
     // Nettoyer la version (remplacer '+' par '-')
     const cleanedVersion: string = currentVersion.replace(/\+/g, '-');
@@ -11,13 +15,22 @@ async function run(): Promise<void> {
     // Afficher la version nettoyée
     console.log(`Cleaned version: ${cleanedVersion}`);
 
-    // Définir la variable d'environnement pour une utilisation ultérieure
-    setOutput('CleanedVersion', cleanedVersion);
-  } catch (error) {
-    console.error('Une erreur s\'est produite:', error);
-    // En cas d'erreur, marquer la tâche comme échouée
-    process.exitCode = TaskResult.Failed;
+    // End of task set ouput variables
+    tl.setVariable("CleanedVersion", cleanedVersion, false, true);
+    tl.setResult(tl.TaskResult.Succeeded, "");
+
   }
+//   catch (error) {
+//     console.error('Une erreur s\'est produite:', error);
+//     // En cas d'erreur, marquer la tâche comme échouée
+//     process.exitCode = TaskResult.Failed;
+//   }
+  catch (err: unknown) {
+    if (err instanceof Error) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+  }
+
 }
 
 run();
